a33f1343b32e9cf3dec84108aa31e26acdb9d394
kerlysantos <santoskerly48@gmail.com>
2023-01-30 12:46:40 -0300

    actualizando el nombre

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1> Probando subida de app react - this is my App :) </h1>
+        <h1> Probando subida de app react kerly santos - this is my App :) </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
